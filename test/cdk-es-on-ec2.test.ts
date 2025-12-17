8b4d48cd02674e5caa596b444555a8a4f10d5c9e
eno <7370762+eno89@users.noreply.github.com>
2025-12-17 09:21:32 +0900

    init cdk

---
+++ test/cdk-es-on-ec2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkEsOnEc2 from '../lib/cdk-es-on-ec2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-es-on-ec2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkEsOnEc2.CdkEsOnEc2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
